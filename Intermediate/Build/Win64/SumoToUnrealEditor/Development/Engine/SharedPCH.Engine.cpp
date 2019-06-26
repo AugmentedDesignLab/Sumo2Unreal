c054ebba9196812444b67b847794662642699764
Ishaan Anil Paranjape <iparanja@crm.ucsc.edu>
2019-06-26 16:17:38 -0700

    Changes to resolve repeated class definitions and dependency statements in build and uplugin file

--- Intermediate/Build/Win64/SumoToUnrealEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Intermediate/Build/Win64/SumoToUnrealEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/iparanja/Documents/Unreal Projects/MyProject01/Sumo2Unreal/Intermediate/Build/Win64/SumoToUnrealEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/iparanja/Documents/Unreal Projects/NewS2U/Sumo2Unreal/Intermediate/Build/Win64/SumoToUnrealEditor/Development/Engine/SharedPCH.Engine.h"
