e0473638ae0ade3f9604e3c8715c073398122505
jawadefaj <jawadefaj006@gmail.com>
2019-11-13 18:17:01 -0800

    Wheeled Vehicle Object class created

---
+++ Source/SumoToUnreal/WheeledVehicleObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "WheeledVehicleObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUMOTOUNREAL_API AWheeledVehicleObject : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
