2a0bc19a9375444a7c8cc5d83493e021898b9413
jawadefaj <jawadefaj006@gmail.com>
2020-03-04 14:23:32 -0800

    vehicle model and scenegenerator integration

---
+++ Source/SumoToUnreal/ScenarioGenerator.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScenarioGenerator.h"
+
+// Sets default values
+AScenarioGenerator::AScenarioGenerator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AScenarioGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AScenarioGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
